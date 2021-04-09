f0b4c767f58a8be2ac7e58ac9cb07676c674fc65
raresGabriel01 <baesu_r@yahoo.com>
2021-04-09 08:55:08 -0700

    commit

---
+++ src/Examen.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Examen {
+    Date data;
+
+    Examen(Date data) {
+        this.data = data;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+}
